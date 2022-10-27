814604ecba70459cfe084a88f782f23b5aaca5be
imFighting18 <114118591+gabriel-santos18@users.noreply.github.com>
2022-10-27 11:17:43 -0300

    .

--- src/main/java/com/imfighting/tutorial/Main.java
+++ src/main/java/com/imfighting/tutorial/Main.java
@@ -4,20 +4,24 @@ import com.imfighting.commands.ConfigCommand;
 import com.imfighting.commands.HealthCommand;
 import com.imfighting.commands.TestCommand;
 import com.imfighting.commands.TesteCommand;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
@@ -26,7 +30,13 @@ public final class Main extends JavaPlugin implements Listener {
     public void onEnable() {
         /*getConfig().options().copyDefaults();
         saveDefaultConfig();
-        ArmorStand armorStand = (ArmorStand) Bukkit.getWorld("world").spawnEntity(new Location(Bukkit.getWorld("world"), -42, 68, 40), EntityType.ARMOR_STAND);*/
+        ArmorStand armorStand = (ArmorStand) Bukkit.getWorld("world").spawnEntity(new Location(Bukkit.getWorld("world"), -42, 68, 40), EntityType.ARMOR_STAND);
+        Bukkit.getWorld("world").getBlockAt(1, 1, 1).setType(Material.LAVA);*/
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
 
     }
 }
