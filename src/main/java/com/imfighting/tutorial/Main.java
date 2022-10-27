f023e0d01f7aaf740207c10a2ca9c823918d1c0b
imFighting18 <114118591+gabriel-santos18@users.noreply.github.com>
2022-10-27 20:47:43 -0300

    .

--- src/main/java/com/imfighting/tutorial/Main.java
+++ src/main/java/com/imfighting/tutorial/Main.java
@@ -4,12 +4,18 @@ import com.imfighting.commands.ConfigCommand;
 import com.imfighting.commands.HealthCommand;
 import com.imfighting.commands.TestCommand;
 import com.imfighting.commands.TesteCommand;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.minecraft.server.v1_8_R3.ChatComponentText;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerListHeaderFooter;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 import org.bukkit.*;
+import org.bukkit.Color;
 import org.bukkit.block.Block;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -17,26 +23,25 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.awt.*;
+import java.lang.reflect.Field;
 
 public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        /*getConfig().options().copyDefaults();
-        saveDefaultConfig();
-        ArmorStand armorStand = (ArmorStand) Bukkit.getWorld("world").spawnEntity(new Location(Bukkit.getWorld("world"), -42, 68, 40), EntityType.ARMOR_STAND);
-        Bukkit.getWorld("world").getBlockAt(1, 1, 1).setType(Material.LAVA);*/
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
-
-    }
 }
