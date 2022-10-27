2d6e1d9c8b9ecfe51c6879edfc124617db123e44
imFighting18 <114118591+gabriel-santos18@users.noreply.github.com>
2022-10-27 08:51:29 -0300

    Initial commit

---
+++ src/main/java/com/imfighting/commands/HealthCommand.java
@@ -0,0 +1,23 @@
+package com.imfighting.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HealthCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("§aSua vida foi restaurada!");
+            player.setHealth(20);
+        }
+
+
+        return false;
+    }
+}
---
+++ src/main/java/com/imfighting/tutorial/Main.java
@@ -0,0 +1,30 @@
+package com.imfighting.tutorial;
+
+import com.imfighting.commands.HealthCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("heal").setExecutor(new HealthCommand());
+    }
+
+   /* @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.BLUE + "Stop moving, you're frozen!");
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e) {
+        e.getPlayer().sendMessage("You are throwing egg!");
+    }*/
+}
